fd9c27f27c6fab8ad686d4bd62951bd794f3c177
Ahmed Attia <ahmadyattia@gmail.com>
2026-08-07 17:31:30 +0300

    login frontend

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { useAuthStore } from "../data/useAuthState";
+import { useNavigate } from "react-router";
+import styles from "../styles/Login.module.css";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const login = useAuthStore((state: any) => state.login);
+  const isAuthenticated = useAuthStore((state: any) => state.isAuthenticated);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const data = await login(username, password);
+
+      if (data.success) {
+        navigate("/home", { replace: true });
+      } else if (data.usernameError) {
+        setError(data.usernameError);
+      } else if (data.passwordError) {
+        setError("Invalid password.");
+      } else if (data.error) {
+        setError(data.error);
+      }
+    } catch (error: any) {
+      setError(error.message);
+    }
+  };
+
+  // useEffect(() => {
+  //   if (isAuthenticated) {
+  //     navigate("/home", { replace: true });
+  //   }
+  // }, [isAuthenticated]);
+
+  return (
+    <>
+      <h1 className={styles["header"]}>Login</h1>
+      <form className={styles["form"]} onSubmit={handleSubmit} action="">
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+        {error && <p className={styles["error"]}>*{error}</p>}
+      </form>
+    </>
+  );
+};
+
+export default Login;
