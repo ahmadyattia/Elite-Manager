44edc895968d5434f7784e74a10ef1111c3fca7a
Ahmed Attia <ahmadyattia@gmail.com>
2026-08-07 21:41:30 +0300

    .

--- backend/src/controllers/loginController.js
+++ backend/src/controllers/loginController.js
@@ -43,32 +43,18 @@ export const loginController = async (req, res) => {
     // Check if the current environment is production
     const isProduction = process.env.NODE_ENV === "production";
 
-    // res.cookie("token", token, {
-    //   httpOnly: true,
-    //   secure: isProduction, // true means only passes over HTTPS (turn off for local development)
-    //   maxAge: 60 * 15 * 1000,
-    //   sameSite: isProduction ? "none" : "lax",
-    // });
-
-    // res.cookie("logged_in", loggedInToken, {
-    //   httpOnly: false,
-    //   secure: isProduction,
-    //   maxAge: 60 * 15 * 1000,
-    //   sameSite: isProduction ? "none" : "lax",
-    // });
-
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true, // true means only passes over HTTPS (turn off for local development)
+      secure: isProduction, // true means only passes over HTTPS (turn off for local development)
       maxAge: 60 * 15 * 1000,
-      sameSite: "none",
+      sameSite: isProduction ? "none" : "lax",
     });
 
     res.cookie("logged_in", loggedInToken, {
       httpOnly: false,
-      secure: true,
+      secure: isProduction,
       maxAge: 60 * 15 * 1000,
-      sameSite: "none",
+      sameSite: isProduction ? "none" : "lax",
     });
 
     res.status(200).json({
