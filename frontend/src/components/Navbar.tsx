a9abd80c403d92c42fec1d86efaa6d9c200190ab
Ahmed Attia <ahmadyattia@gmail.com>
2026-08-11 13:15:09 +0300

    add admin tag to the navbar and restyle links

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,11 +4,17 @@ import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
   const isAuthenticated = useAuthStore((state: any) => state.isAuthenticated);
+  const isAdminAuthenticated = useAuthStore(
+    (state: any) => state.isAdminAuthenticated,
+  );
 
   return (
     <nav className={styles["navbar"]}>
       {isAuthenticated && <Link to={"home"}>Home</Link>}
       {!isAuthenticated && <Link to={"login"}>Login</Link>}
+      {isAdminAuthenticated && (
+        <span className={styles["admin-tag"]}>admin</span>
+      )}
     </nav>
   );
 };
